06d596b914d1ba8e0f7db0bb6a5888b5be7f9d09
Jakub Dutkiewicz <jak.dut@gmail.com>
2020-11-18 14:25:33 +0100

    Update

--- src/test/java/com/example/springbootmapCovidConfirmed/SpringBootMapCovidConfirmedTests.java
+++ src/test/java/com/example/springbootmapCovidConfirmed/SpringBootMapCovidConfirmedTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootMapInitApplicationTests {
+class SpringBootMapCovidConfirmedTests {
 
 	@Test
 	void contextLoads() {
