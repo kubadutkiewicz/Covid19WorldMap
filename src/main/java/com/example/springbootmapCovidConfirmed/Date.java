77c195f0b6cf0663756dc3a3ad27fd672249a20a
Jakub Dutkiewicz <jak.dut@gmail.com>
2021-03-03 14:12:31 +0100

    Update

--- src/main/java/com/example/springbootmapCovidConfirmed/Date.java
+++ src/main/java/com/example/springbootmapCovidConfirmed/Date.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Date {
 
-    private static final String date = "1/13/21";
+    private static final String date = "3/2/21";
 
     public String getDate() {
         return date;
